f479b936d23b419ba7e963d7bf0cfc50ac2ac31e
Jonathan E. McMahon <jmcmahon32@gatech.edu>
2018-04-14 19:42:43 -0400

    P1.05 - Reverse list

---
+++ p1.05.pl
@@ -0,0 +1,16 @@
+% 1.04 (*) Find the number of elements of a list.
+% Problem source: 
+% https://sites.google.com/site/prologsite/prolog-problems/
+% 
+% % Example usage:
+% ?- reverse_list(L, [1,2,3,4,5,6,7]).
+% L = [7, 6, 5, 4, 3, 2, 1]
+
+reverse_list(Z,[], Z).
+
+reverse_list(L, [H|T], R) :-
+    reverse_list(L, T, [H|R]).
+
+reverse_list(L, [Head|Tail]) :-
+    reverse_list(L, Tail, [Head]).
+
