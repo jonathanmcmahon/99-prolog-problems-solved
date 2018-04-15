825555f753969a0c857aee9577321710d5d5d151
Jonathan E. McMahon <jmcmahon32@gatech.edu>
2018-04-15 15:36:43 -0400

    P1.10 - Run-length encoding

---
+++ p1.10.pl
@@ -0,0 +1,23 @@
+% 1.10 (*) Run-length encoding of a list.
+% Problem source: 
+% https://sites.google.com/site/prologsite/prolog-problems/
+% 
+% Example usage:
+% ?- run_length_encode([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+% X = [[4, a], [1, b], [2, c], [2, a], [1, d], [4, e]]
+
+run_length_encode(E, [], S, L, C) :-
+    length([E|S], Slen),
+    append(L, [[Slen, E]], C).
+
+run_length_encode(E, [H|T], S, L, C) :- 
+    (   E == H ->  
+    	run_length_encode(H, T, [E|S], L, C)
+    ;   length([E|S], Slen),
+        append(L, [[Slen, E]], Ln),
+        run_length_encode(H, T, [], Ln, C)
+    ).
+
+run_length_encode([H|T],C) :- 
+    run_length_encode(H,T, [], [], C).
+
